35fcc239ef7ad30a6e5e5d12dad353f6f5ecbd71
sivadasan <kc.sivadasan@gmail.com>
2016-09-25 19:46:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/articleone', function (req, res) {
+app.get('/articleone', function (req, res) {
   res.send('articleone');
 });
 app.get('/ui/articletwo', function (req, res) {
